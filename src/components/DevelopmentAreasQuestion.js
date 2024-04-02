b78b098e4ee52d75016f656419b1ee07cefdbadf
Ryan Coles <ryanc2811@live.co.uk>
2024-04-02 09:31:53 +0100

    generating questions

--- src/components/DevelopmentAreasQuestion.js
+++ src/components/DevelopmentAreasQuestion.js
@@ -8,11 +8,15 @@ import { getAuth } from "firebase/auth";
 import UserTnaContext from './UserTnaContext';
 
 
+import CircularProgress from '@mui/material/CircularProgress';
+
 
 function DevelopmentAreasQuestion() {
     const { userTnaId: docId, setUserTnaId, businessAreaId } = useContext(UserTnaContext);
     const [selectedOptions, setSelectedOptions] = useState([]);
     const [developmentAreas, setDevelopmentAreas] = useState([]);
+    // Add loading state
+    const [isLoading, setIsLoading] = useState(false);
 
     const theme = useTheme();
     const navigate = useNavigate();
@@ -60,6 +64,8 @@ function DevelopmentAreasQuestion() {
         const auth = getAuth();
         const user = auth.currentUser;
         const userUID = user ? user.uid : null;
+
+        setIsLoading(true); // Start loading
         if (selectedOptions.length > 0) {
             try {
                 // Your existing code to get the user's ID and check docId
@@ -72,7 +78,7 @@ function DevelopmentAreasQuestion() {
                         });
 
                     }
-                    navigate('/next-question-path');
+
                 } else {
                     alert('No user is signed in.');
                     navigate('/login');
@@ -83,82 +89,119 @@ function DevelopmentAreasQuestion() {
         } else {
             alert('Please select at least one option before submitting.');
         }
+
+
+        console.log(selectedOptions);
+        const user_profile = {
+            business_area: businessAreaId,
+            development_areas: selectedOptions,
+        };
+        // Example POST request with fetch
+        fetch('https://recommendations-bpdibe4qla-ez.a.run.app/recommendT', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(user_profile),
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                // Handle the response data
+                navigate('/generate-questions', { state: { questions: data.recommendedQuestions } });
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+            })
+            .finally(() => {
+                setIsLoading(false); // Stop loading
+            });
+
     };
     return (
+
         <Box sx={{ display: 'flex', flexDirection: 'column', gap: 2, alignItems: 'center', m: 4 }}>
-            <Paper
-                elevation={0}
-                square
-                sx={{
-                    border: '2px solid #6200EE',
-                    borderRadius: 2,
-                    padding: '20px',
-                    marginTop: '20px',
-                    maxWidth: '947px',
-                    width: { xs: '100%', sm: '50%', md: '25%' },
-                }}
-            >
-                <Box textAlign="center" sx={{ fontSize: { xs: '0.7rem', sm: '0.8rem', md: '1rem' }, color: theme.palette.primary.main }}>
-                    <h2>
-                        <span style={{ fontWeight: 'normal' }}>WHAT</span> KEY AREAS <span style={{ fontWeight: 'normal' }}>DO YOU WANT TO DEVELOP? (SELECT ALL THAT APPLY)</span>
-                    </h2>
-                </Box>
-            </Paper>
-            <Box
-                sx={{
-                    overflowY: 'auto', // Enable vertical scroll
-                    maxHeight: { xs: '300px', sm: '400px', md: '500px' }, // Adjust this value based on your needs
-                    width: { xs: '100%', sm: '40%', md: '20%' },
-                    padding: '10px',
-                }}
-            >
-                <Grid container spacing={2} justifyContent="center">
+            {isLoading ? (
+                <CircularProgress />
+            ) : (
+                <Box sx={{ display: 'flex', flexDirection: 'column', gap: 2, alignItems: 'center', m: 4 }}>
+                    <Paper
+                        elevation={0}
+                        square
+                        sx={{
+                            border: '2px solid #6200EE',
+                            borderRadius: 2,
+                            padding: '20px',
+                            marginTop: '20px',
+                            maxWidth: '947px',
+                            width: { xs: '100%', sm: '50%', md: '25%' },
+                        }}
+                    >
+                        <Box textAlign="center" sx={{ fontSize: { xs: '0.7rem', sm: '0.8rem', md: '1rem' }, color: theme.palette.primary.main }}>
+                            <h2>
+                                <span style={{ fontWeight: 'normal' }}>WHAT</span> KEY AREAS <span style={{ fontWeight: 'normal' }}>DO YOU WANT TO DEVELOP? (SELECT ALL THAT APPLY)</span>
+                            </h2>
+                        </Box>
+                    </Paper>
+                    <Box
+                        sx={{
+                            overflowY: 'auto', // Enable vertical scroll
+                            maxHeight: { xs: '300px', sm: '400px', md: '500px' }, // Adjust this value based on your needs
+                            width: { xs: '100%', sm: '40%', md: '20%' },
+                            padding: '10px',
+                        }}
+                    >
+                        <Grid container spacing={2} justifyContent="center">
 
-                    {developmentAreas.map((area, index) => (
-                        <Grid item xs={6} key={index}>
-                            <Button
-                                key={area}
-                                variant={selectedOptions.includes(area) ? "contained" : "outlined"}
-                                sx={{
-                                    justifyContent: 'space-between',
-                                    pl: 3,
-                                    pr: 3,
-                                    width: '100%', // Make buttons full width inside the scrollable area
-                                    height: '120px',
-                                    fontSize: { xs: '0.6rem', sm: '0.7rem', md: '.8rem' },
-                                    minWidth: '150px',
+                            {developmentAreas.map((area, index) => (
+                                <Grid item xs={6} key={index}>
+                                    <Button
+                                        key={area}
+                                        variant={selectedOptions.includes(area) ? "contained" : "outlined"}
+                                        sx={{
+                                            justifyContent: 'space-between',
+                                            pl: 3,
+                                            pr: 3,
+                                            width: '100%', // Make buttons full width inside the scrollable area
+                                            height: '120px',
+                                            fontSize: { xs: '0.6rem', sm: '0.7rem', md: '.8rem' },
+                                            minWidth: '150px',
 
-                                    '&.MuiButton-contained': {
-                                        color: theme.palette.common.white,
-                                    },
-                                    mb: 2, // Add some space between buttons
-                                }}
-                                onClick={() => handleSelect(area)}
-                            >
-                                {area}
-                            </Button>
+                                            '&.MuiButton-contained': {
+                                                color: theme.palette.common.white,
+                                            },
+                                            mb: 2, // Add some space between buttons
+                                        }}
+                                        onClick={() => handleSelect(area)}
+                                    >
+                                        {area}
+                                    </Button>
+                                </Grid>
+                            ))}
                         </Grid>
-                    ))}
-                </Grid>
-            </Box>
+                    </Box>
 
-            <Button
-                variant="contained"
-                color="secondary"
-                sx={{
-                    mt: 4,
-                    width: { xs: '50%', sm: '40%', md: '15%' },
-                    height: '50px',
-                    fontSize: { xs: '0.9rem', sm: '1rem', md: '1rem' },
-                    minWidth: '150px',
-                    color: 'white',
-                    fontWeight: 'bold'
-                }}
-                onClick={handleSubmit}
-            >
-                Next
-            </Button>
+                    <Button
+                        variant="contained"
+                        color="secondary"
+                        sx={{
+                            mt: 4,
+                            width: { xs: '50%', sm: '40%', md: '15%' },
+                            height: '50px',
+                            fontSize: { xs: '0.9rem', sm: '1rem', md: '1rem' },
+                            minWidth: '150px',
+                            color: 'white',
+                            fontWeight: 'bold'
+                        }}
+                        onClick={handleSubmit}
+                    >
+                        Next
+                    </Button>
+                </Box>
+            )}
+
+
         </Box>
+
     );
 }
 
